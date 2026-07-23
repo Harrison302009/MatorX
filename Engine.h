8baa12eda21280e477d8fe3c642a033d3e6bc2c3
Harrison John <harrisjohnu@gmailo.com>
2026-07-23 19:01:32 +0100

    built the engine head

--- Engine.h
+++ Engine.h
@@ -1,14 +1,22 @@
 #define SDL_MAIN_HANDLED
 #pragma once
+#include <SDL2/SDL.h>
 
 class Engine
 {
 public:
 	Engine();
 	~Engine();
-
+	bool init(char* title, int width, int height);
+	void handleEvents();
+	void update();
+	void render();
+	void clean();
+	void isRunning() {}
 private:
-
+	bool m_running;
+	SDL_Window* m_window;
+	SDL_Renderer* m_renderer;
 };
 
 Engine::Engine()
